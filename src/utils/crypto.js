f771fab9a528bd4925361dba546624b61d62b81e
caglky <s334458@studenti.polito.it>
2026-04-24 19:54:18 +0200

    Update crypto.js

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,19 +1,19 @@
 import CryptoJS from "crypto-js";
 
-const secret_key = "secret";  //şifreleme ve çözme için kullanılan anahtar 
+const secret_key = "secret"; 
 
 export function encryptData (data){ 
-    const jsonString = JSON.stringify(data); //gelen objeyi string'e çevirir
-    const encryted = CryptoJS.AES.encrypt(jsonString, secret_key).toString(); //AES ile şifreler, string olarak döndürür
+    const jsonString = JSON.stringify(data);
+    const encryted = CryptoJS.AES.encrypt(jsonString, secret_key).toString(); 
     return encryted;
 }
 
 export function decryptData (encryptData){
     try{
-        const decryted = CryptoJS.AES.decrypt(encryptData, secret_key); //şifreli veri çözülüyor
-        const decryptedString = decryted.toString(CryptoJS.enc.Utf8); //tekrar string alınıyor 
+        const decryted = CryptoJS.AES.decrypt(encryptData, secret_key);
+        const decryptedString = decryted.toString(CryptoJS.enc.Utf8); 
         if (!decryptedString){
-            return null; //veri bozuksa null döner 
+            return null;  
         }
         return JSON.parse(decryptedString);
     } catch (error){
